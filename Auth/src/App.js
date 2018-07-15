9e996e25eb855274504a3ef820ca5c62d5e31551
Rajat Kasliwal <kasliwalrajat1708@gmail.com>
2018-07-15 18:39:08 +0530

    Add firebase - Add partially Login Form with Input Text

--- Auth/src/App.js
+++ Auth/src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm'
 
 class App extends Component {
+
+  componentWillMount() {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyDFsd6NfoT7OrNbLTT1Oy8QTDKa3mLyIqM',
+      authDomain: 'reactauth-1ed44.firebaseapp.com',
+      databaseURL: 'https://reactauth-1ed44.firebaseio.com',
+      projectId: 'reactauth-1ed44',
+      storageBucket: '',
+      messagingSenderId: '721212971254'
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
+
   render() {
     return (
       <View>
         <Header headerText='Authentication' />
-        <Text>Auth</Text>
+        <LoginForm />
       </View>
     );
   }
